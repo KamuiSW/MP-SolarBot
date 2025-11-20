86af7b5f78f874fac8679926aa9bdb65ad0b8bc0
Kamui <kangym727@gmail.com>
2025-11-20 16:57:12 +0100

    mapping and simulator

---
+++ Software/src/Robot/Core/mapping.cpp
@@ -0,0 +1,68 @@
+﻿#include <vector>
+
+struct Cell {
+    bool isEdge = false;
+    bool onPath = false;
+};
+
+class PanelMap {
+public:
+    int w, h;
+    std::vector<std::vector<Cell>> grid;
+
+    PanelMap(int width, int height) : w(width), h(height) {
+        grid.resize(h, std::vector<Cell>(w));
+    }
+
+    void markEdge(int x, int y) { grid[y][x].isEdge = true; }
+    void markPath(int x, int y) { grid[y][x].onPath = true; }
+};
+
+class RobotLogic {
+public:
+    int x = 0, y = 0;
+
+    void moveUp() { y--; }
+    void moveDown() { y++; }
+    void moveLeft() { x--; }
+    void moveRight() { x++; }
+
+    void mapRectangle(PanelMap& map) {
+        //Top edge
+        for (int i = 0; i < map.w; i++) { x = i; y = 0; map.markEdge(x, y); }
+
+        //Right edge
+        for (int i = 0; i < map.h; i++) { x = map.w - 1; y = i; map.markEdge(x, y); }
+
+        //Bottom edge
+        for (int i = map.w - 1; i >= 0; i--) { x = i; y = map.h - 1; map.markEdge(x, y); }
+
+        //Left edge
+        for (int i = map.h - 1; i >= 0; i--) { x = 0; y = i; map.markEdge(x, y); }
+    }
+
+    void planSpiral(PanelMap& map) {
+        int left = 1, right = map.w - 2;
+        int top = 1, bottom = map.h - 2;
+
+        while (left <= right && top <= bottom) {
+            //ight
+            for (int i = left; i <= right; i++) map.markPath(i, top);
+            top++;
+
+            //Down
+            for (int i = top; i <= bottom; i++) map.markPath(right, i);
+            right--;
+
+            //Left
+            if (top <= bottom)
+                for (int i = right; i >= left; i--) map.markPath(i, bottom);
+            bottom--;
+
+            //Up
+            if (left <= right)
+                for (int i = bottom; i >= top; i--) map.markPath(left, i);
+            left++;
+        }
+    }
+};
---
+++ Software/src/Robot/Core/simulator.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+#include "mapping.cpp"
+
+using namespace std;
+
+class Simulator {
+public:
+    PanelMap map;
+    RobotLogic robot;
+
+    Simulator(int w, int h) : map(w, h) {}
+
+    void draw() {
+        system("cls");
+        for (int y = 0; y < map.h; y++) {
+            for (int x = 0; x < map.w; x++) {
+                if (x == robot.x && y == robot.y) cout << "R";
+                else if (map.grid[y][x].isEdge) cout << "#";
+                else if (map.grid[y][x].onPath) cout << "*";
+                else cout << ".";
+            }
+            cout << "\n";
+        }
+        this_thread::sleep_for(chrono::milliseconds(120));
+    }
+
+    void simulateMapping() {
+        for (robot.x = 0, robot.y = 0; robot.x < map.w; robot.x++) {
+            map.markEdge(robot.x, robot.y);
+            draw();
+        }
+
+        for (robot.y = 0; robot.y < map.h; robot.y++) {
+            robot.x = map.w - 1;
+            map.markEdge(robot.x, robot.y);
+            draw();
+        }
+
+        for (robot.x = map.w - 1; robot.x >= 0; robot.x--) {
+            robot.y = map.h - 1;
+            map.markEdge(robot.x, robot.y);
+            draw();
+        }
+
+        for (robot.y = map.h - 1; robot.y >= 0; robot.y--) {
+            robot.x = 0;
+            map.markEdge(robot.x, robot.y);
+            draw();
+        }
+    }
+
+    void simulateSpiral() {
+        int left = 1, right = map.w - 2;
+        int top = 1, bottom = map.h - 2;
+
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) {
+                robot.x = i; robot.y = top;
+                map.markPath(robot.x, robot.y);
+                draw();
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                robot.x = right; robot.y = i;
+                map.markPath(robot.x, robot.y);
+                draw();
+            }
+            right--;
+
+            if (top <= bottom)
+                for (int i = right; i >= left; i--) {
+                    robot.x = i; robot.y = bottom;
+                    map.markPath(robot.x, robot.y);
+                    draw();
+                }
+            bottom--;
+
+            if (left <= right)
+                for (int i = bottom; i >= top; i--) {
+                    robot.x = left; robot.y = i;
+                    map.markPath(robot.x, robot.y);
+                    draw();
+                }
+            left++;
+        }
+    }
+};
+
+int main() {
+    Simulator sim(30, 12);
+
+    sim.simulateMapping();
+    sim.simulateSpiral();
+
+    cout << "done\n";
+    return 0;
+}
